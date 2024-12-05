ac8ea132d843b6da6d42627d1a31dabf194272c7
Fernando Igor <fernandoigor.dantas@gmail.com>
2024-12-04 21:50:48 -0300

    orgazation of main and app widget

---
+++ lib/src/app_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chess versus',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
